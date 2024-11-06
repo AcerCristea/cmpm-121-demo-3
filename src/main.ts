d374f950163617ad95cd91b350afd8d8787db9c9
AcerCristea <acercristea@gmail.com>
2024-11-05 22:23:52 -0800

    L16 Complete

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
-// todo
+const button = document.createElement("button");
+button.textContent = "Click Me!";
+
+button.onclick = () => {
+  alert("you clicked the button!");
+};
+
+document.body.appendChild(button);
